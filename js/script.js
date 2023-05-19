cbacca172ea6b759c8d03249d7258884b6f25ba7
Lucas Henrique <lucashcoliveira1337@gmail.com>
2023-05-18 21:53:12 -0300

    projeto finalziado + js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// ativar links do menu//
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento //
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const element = document.getElementById(parametro);
+  if (element) {
+    element.checked = true;
+  }
+
+  console.log(element);
+}
+
+parametros.forEach(ativarProduto);
+
+
+// Animação
+if(window.SimpleAnime){
+  new SimpleAnime()
+}
